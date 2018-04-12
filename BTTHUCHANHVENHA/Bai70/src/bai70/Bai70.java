a221a325efea5a1734e2289437e3a058261d66b0
=buihuubang <=>
2018-04-12 17:24:30 +0700

    12/4

--- BTTHUCHANHVENHA/Bai70/src/bai70/Bai70.java
+++ BTTHUCHANHVENHA/Bai70/src/bai70/Bai70.java
@@ -3,20 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package bai69;
+package bai70;
 
 /**
  *
  * @author bang
  */
-public class Bai69 {
+public class Bai70 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        new Tong().setVisible(true);
+        new TinhTien().setVisible(true);
     }
     
 }
--- BTTHUCHANHVENHA/Bai71/src/bai71/Bai71.java
+++ BTTHUCHANHVENHA/Bai71/src/bai71/Bai71.java
@@ -3,20 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package bai69;
+package bai71;
 
 /**
  *
  * @author bang
  */
-public class Bai69 {
+public class Bai71 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        new Tong().setVisible(true);
+        new HinhTron().setVisible(true);
     }
     
 }
