c792909be6e56ae658ec43fd2a671760ba5e75a5
=buihuubang <=>
2018-04-12 21:30:46 +0700

    12/4

---
+++ BTTHUCHANHVENHA/Bai74/src/bai74/Bai74.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai74;
+
+/**
+ *
+ * @author bang
+ */
+public class Bai74 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new HoTen().setVisible(true);
+    }
+    
+}
