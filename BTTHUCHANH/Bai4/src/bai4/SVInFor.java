51ef36545fa304c4c25cc7e8a7546e4c5f681bb9
=buihuubang <=>
2018-04-23 23:09:55 +0700

    23/4

---
+++ BTTHUCHANH/Bai4/src/bai4/SVInFor.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai4;
+
+/**
+ *
+ * @author bang
+ */
+public class SVInFor {
+    private String MSSV;
+    private String HoTen;
+    private String NamSinh;
+    private String Lop;
+    
+    public void setMSSV(String MSSV){
+        this.MSSV = MSSV;
+    }
+    public String getMSSV(){
+        return MSSV;
+    }
+    
+    public void setHoTen(String HoTen){
+        this.HoTen = HoTen;
+    }
+    public String getHoTen(){
+        return HoTen;
+    }
+    
+    public void setNamSinh(String NamSinh){
+        this.NamSinh = NamSinh;
+    }
+    public String getNamSinh(){
+        return NamSinh;
+    }
+    
+    public void setLop(String Lop){
+        this.Lop = Lop;
+    }
+    public String getLop(){
+        return Lop;
+    }
+}
